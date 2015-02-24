3a713201db29c43f63d8d6119488e726c9f71bb8
Michael Krasnovsky <mkrasnovsky@gmail.com>
2015-02-24 20:16:24 +0100

    Reformatted code (4 tabs indention)

--- app/scripts/component/ui/click-counter.js
+++ app/scripts/component/ui/click-counter.js
@@ -4,35 +4,35 @@ var defineComponent = require('flight/lib/component');
 var template = require('default-click-counter.hgn');
 
 function clickCounter() {
-  this.attributes({
-    clickableSelector: 'button',
-    noteSelector: '.note span',
-    templateName: 'default-click-counter'
-  });
+    this.attributes({
+        clickableSelector: 'button',
+        noteSelector: '.note span',
+        templateName: 'default-click-counter'
+    });
 
-  this.onClick = function (e) {
-    this.counter++;
+    this.onClick = function (e) {
+        this.counter++;
 
-    this.button.innerHTML = this.counter;
-    this.note.innerHTML = new Date(e.timeStamp);
-    this.trigger('uiClickedMore');
-  };
+        this.button.innerHTML = this.counter;
+        this.note.innerHTML = new Date(e.timeStamp);
+        this.trigger('uiClickedMore');
+    };
 
-  this.render = function () {
-    console.log('rendered');
-    var context = {label: 0, now: 'Never fired!'};
-    this.node.innerHTML = template(context);
-  };
+    this.render = function () {
+        console.log('rendered');
+        var context = {label: 0, now: 'Never fired!'};
+        this.node.innerHTML = template(context);
+    };
 
-  this.after('initialize', function () {
-    this.counter = 0;
-    this.render();
+    this.after('initialize', function () {
+        this.counter = 0;
+        this.render();
 
-    this.button = this.select('clickableSelector')[0];
-    this.note = this.select('noteSelector')[0];
+        this.button = this.select('clickableSelector')[0];
+        this.note = this.select('noteSelector')[0];
 
-    this.on('click', {clickableSelector: this.onClick});
-  });
+        this.on('click', {clickableSelector: this.onClick});
+    });
 }
 
 module.exports = defineComponent(clickCounter);
--- webpack.config.js
+++ webpack.config.js
@@ -6,78 +6,79 @@ var ResolverPlugin = webpack.ResolverPlugin;
 var CommonsChunkPlugin = webpack.optimize.CommonsChunkPlugin;
 
 var conf = {
-  entry: {
-    app: './app/scripts/app.js',
-    vendor: [
-      'jquery',
-      'flight',
-      'hogan.js',
-      'normalize-css'
+    entry: {
+        app: './app/scripts/app.js',
+        vendor: [
+            'jquery',
+            'flight',
+            'hogan.js',
+            'normalize-css'
+        ]
+    },
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: '[hash].bundle.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.hgn$/,
+                loader: 'mustache'
+            },
+            {
+                test: /\.woff(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+                loader: 'url-loader?limit=10000&minetype=application/font-woff'
+            },
+            {
+                test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+                loader: 'file'
+            },
+            {
+                test: /\.scss$/,
+                loader: 'style!css!autoprefixer?browsers=last 2 version!sass?outputStyle=expanded'
+            },
+            {
+                test: /\.css$/,
+                loader: 'style!css!autoprefixer?browsers=last 2 version'
+            },
+            {
+                test: /\.(jpg|jpeg)$/,
+                loader: 'file' //?name=[name].[ext]' // prevents from long term caching
+            },
+            {
+                test: /\.png$/,
+                loader: 'url?mimetype=image/png'
+            },
+            {
+                test: /favicon.ico$/,
+                loader: 'file?name=favicon.ico'
+            }
+        ]
+    },
+    resolve: {
+        modulesDirectories: [
+            'bower_components',
+            'node_modules',
+            'scripts',
+            'templates',
+            'styles',
+            'images'
+        ]
+    },
+    plugins: [
+        new ProvidePlugin({
+            $: 'jquery',
+            jQuery: 'jquery'
+        }),
+        new ResolverPlugin([
+            new ResolverPlugin.DirectoryDescriptionFilePlugin('bower.json', ['main'])
+        ]),
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: 'app/index.html'
+        }),
+        new CommonsChunkPlugin('vendor', '[hash].vendor.bundle.js')
     ]
-  },
-  output: {
-    path: path.join(__dirname, 'dist'),
-    filename: '[hash].bundle.js'
-  },
-  module: {
-    loaders: [
-      {
-        test: /\.hgn$/,
-        loader: 'mustache'
-      },
-      { test: /\.woff(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        loader: 'url-loader?limit=10000&minetype=application/font-woff'
-      },
-      {
-        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        loader: 'file'
-      },
-      {
-        test: /\.scss$/,
-        loader: 'style!css!autoprefixer?browsers=last 2 version!sass?outputStyle=expanded'
-      },
-      {
-        test: /\.css$/,
-        loader: 'style!css!autoprefixer?browsers=last 2 version'
-      },
-      {
-        test: /\.(jpg|jpeg)$/,
-        loader: 'file' //?name=[name].[ext]' // prevents from long term caching
-      },
-      {
-        test: /\.png$/,
-        loader: 'url?mimetype=image/png'
-      },
-      {
-        test: /favicon.ico$/,
-        loader: 'file?name=favicon.ico'
-      }
-    ]
-  },
-  resolve: {
-    modulesDirectories: [
-      'bower_components',
-      'node_modules',
-      'scripts',
-      'templates',
-      'styles',
-      'images'
-    ]
-  },
-  plugins: [
-    new ProvidePlugin({
-      $: 'jquery',
-      jQuery: 'jquery'
-    }),
-    new ResolverPlugin([
-      new ResolverPlugin.DirectoryDescriptionFilePlugin('bower.json', ['main'])
-    ]),
-    new HtmlWebpackPlugin({
-      filename: 'index.html',
-      template: 'app/index.html'
-    }),
-    new CommonsChunkPlugin('vendor', '[hash].vendor.bundle.js')
-  ]
 };
 
 module.exports = conf;
